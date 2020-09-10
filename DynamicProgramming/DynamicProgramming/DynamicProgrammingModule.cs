14db132b7226373f58d1f1765ddd2f9cafb70650
Vijay Prativadi <vijayprativadi@hotmail.com>
2020-09-09 23:02:02 -0400

    Added LIS & LDS

---
+++ DynamicProgramming/DynamicProgramming/DynamicProgrammingModule.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicProgramming
+{
+    public static class DynamicProgrammingModule
+    {
+        public static void LongestIncreasingSubsequence(int[] arr)
+        {
+            IList<IList<int>> LIS = new List<IList<int>>();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                LIS.Add(new List<int>());
+            }
+
+            LIS[0].Add(arr[0]);
+
+            for (int i = 1; i < arr.Length; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    if (arr[j] < arr[i] && LIS[j].Count > LIS[i].Count)
+                    {
+                        LIS[i] = new List<int>(LIS[j]);
+                    }
+                }
+                
+                LIS[i].Add(arr[i]);
+            }
+
+            // LIS Length
+            var lisResult = LIS.OrderByDescending(x => x.Count()).Take(1).ToArray()[0];
+            Console.WriteLine("Length of LIS: " + lisResult.Count());
+
+            StringBuilder sb = new StringBuilder();
+            String SEPARATOR = "";
+            foreach (var item in lisResult)
+            {
+                sb.Append(SEPARATOR);
+                sb.Append(item.ToString());
+                SEPARATOR = ",";
+            }
+
+            // LIS Result
+            Console.WriteLine("Result of LIS: " + sb.ToString());
+        }
+
+        public static void LongestDecreasingSubsequence(int[] arr)
+        {
+            IList<IList<int>> LDS = new List<IList<int>>();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                LDS.Add(new List<int>());
+            }
+
+            LDS[0].Add(arr[0]);
+
+            for (int i = 1; i < arr.Length; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    if (arr[j] > arr[i] && LDS[j].Count > LDS[i].Count)
+                    {
+                        LDS[i] = new List<int>(LDS[j]);
+                    }
+                }
+
+                LDS[i].Add(arr[i]);
+            }
+
+            // LDS Length
+            var ldsResult = LDS.OrderByDescending(x => x.Count()).Take(1).ToArray()[0];
+            Console.WriteLine("Length of LDS: " + ldsResult.Count());
+
+            StringBuilder sb = new StringBuilder();
+            String SEPARATOR = "";
+            foreach (var item in ldsResult)
+            {
+                sb.Append(SEPARATOR);
+                sb.Append(item.ToString());
+                SEPARATOR = ",";
+            }
+
+            // LDS Result
+            Console.WriteLine("Result of LDS: " + sb.ToString());
+        }
+    }
+}
--- DynamicProgramming/DynamicProgramming/Program.cs
+++ DynamicProgramming/DynamicProgramming/Program.cs
@@ -10,6 +10,59 @@ namespace DynamicProgramming
     {
         static void Main(string[] args)
         {
+            //08/29
+            //LCS
+            //Longest Common Substring
+
+
+
+            //08/30
+            //LIS
+            //LDS
+            int[] arr = new int[] { 0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15 };
+            DynamicProgrammingModule.LongestIncreasingSubsequence(arr);
+            DynamicProgrammingModule.LongestDecreasingSubsequence(arr);
+            Console.ReadKey();
+
+            //08/31
+            //Incresing Subseqence with Max Sum
+            //Edit Distance Problem
+            //Minimum cost to reach last cell of the matrix from the first cell
+
+            //09/01
+            //Longest Sequence formed by adjacent numbers in matrix
+            //Knapsack Problem
+            //Partion Problem
+
+            //09/02
+            //Subset Sum Problem
+            //3 Partion Problem
+            //Minimum Sum Partion Problem
+
+            //09/03
+            //Rod Cutting
+            //Maximum Product Rod Cutting
+            //Number of ways to make change
+
+            //09/04
+            //Min Number of Coins for Change
+            //Word Break Problem
+            //Box Stacking Problem
+
+            //09/05
+            //Path in Matrix
+            //Number of time pattern appers in given string as a subsequence 
+            //WildCard Pattern Matching Problem
+
+            //09/06
+            //Palidrome Partioning Min Cuts
+            //Min Jums from source to destination
+            //Maximum Sum of SubSequence with no adjacent elements
+
+            //09/07
+            //Maximum Sum Subarray (Kadane)
+            //Max Profit K Transactions
+
         }
     }
 }
